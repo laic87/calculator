238f1c98039ccc405dd4f2f7e205b4ca8cc97f5c
laic87 <laic87@users.noreply.github.com>
2024-01-09 20:20:03 +0100

    Added Header and a reusable textfield component

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.calculatorapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.calculatorapp.components.InputField
 import com.example.calculatorapp.ui.theme.CalculatorAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,8 +34,69 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Text(text = "Hello")
+                Column(
+                    modifier = Modifier.padding(10.dp)
+                ) {
+                    TopHeader()
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    MainContent()
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun TopHeader(totalPerPerson: Double = 134.0) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
+    ) {
+
+        val total = "%.2f".format(totalPerPerson)
+
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf<String>("0")
+    }
+
+    Surface(modifier = Modifier
+        .padding(2.dp)
+        .fillMaxWidth()
+        .height(200.dp),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, Color.LightGray)
+    ) {
+        Column(
+        ) {
+            InputField(valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true)
+
         }
     }
 }
@@ -40,7 +118,9 @@ fun MyApp(content: @Composable () -> Unit) {
 fun GreetingPreview() {
     CalculatorAppTheme {
         MyApp {
-            Text(text = "test")
+            Column {
+                TopHeader()
+            }
         }
     }
 }
